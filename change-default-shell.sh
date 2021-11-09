e6d14b67a14d7310e5ebddd59c61faec92cae052
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2021-11-09 18:35:19 +0200

    Use wrapped ZSH for user shell

--- change-default-shell.sh
+++ change-default-shell.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "$HOME/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
-sudo chsh -s "$HOME/.nix-profile/bin/zsh" "$(whoami)"
+echo "$HOME/.nix-profile/bin/zsh-wrapped" | sudo tee -a /etc/shells
+sudo chsh -s "$HOME/.nix-profile/bin/zsh-wrapped" "$(whoami)"
