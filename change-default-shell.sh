09bf5c56ec13d58e380f60263267b3b3e65f6208
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2021-11-19 12:38:58 +0200

    Disable SELinux permanently

--- change-default-shell.sh
+++ change-default-shell.sh
@@ -2,6 +2,7 @@
 # Disable SELinux if it's enabled; otherwise we will be unable to login
 # when the new shell is enabled
 sudo setenforce 0 || true
+sudo sed -i "s/SELINUX=.*/SELINUX=disabled/g" /etc/selinux/config || true
 
 echo "$HOME/.nix-profile/bin/zsh-wrapped" | sudo tee -a /etc/shells
 sudo chsh -s "$HOME/.nix-profile/bin/zsh-wrapped" "$(whoami)"
