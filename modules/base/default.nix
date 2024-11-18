57e7e03fdd769f86557c1763c11fff5fdd2fb834
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2024-11-18 16:25:59 +0200

    Make Flatpak apps visible to desktop

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -3,4 +3,10 @@
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Make Flatpak apps available
+  xdg.systemDirs.data = [
+    "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
+    "/var/lib/flatpak/exports/share"
+  ];
 }
