bff138cb750c6285f4b10ca8f5edb37e08680b3b
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2026-05-25 14:36:04 +0300

    Remove old non-NixOS workaround
    Probably was only ever useful for CentOS 7

--- modules/generic-linux/default.nix
+++ modules/generic-linux/default.nix
@@ -3,11 +3,6 @@
 with lib;
 
 {
-  imports = [
-    # Fix for some locale issues with older distros
-    ./multi-glibc-locale-paths.nix
-  ];
-
   # Defaults to make Nix play nicer with non-NixOS distros
   targets.genericLinux.enable = true;
 
